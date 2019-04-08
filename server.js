d4635c0274a97981733dffe7a18ef3269f3042d6
AntonVoronezh <pstut@mail.ru>
2019-04-08 17:15:27 +0300

    add express

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
+const express = require("express");
 
+const app = express();
+
+const artists = [
+  { id: 1, name: "Metallica" },
+  { id: 2, name: "Iron Maiden" },
+  { id: 3, name: "Deep Purple" }
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello API");
+});
+
+app.get("/artists", (req, res) => {
+  res.send(artists);
+});
 
